221aca87b6d2f027ce2adda437a379efe4078b70
Thogaruchsesti-Hemanth <saihemanth225@g,ail.com>
2025-06-06 23:48:56 +0530

    Temperature conversion screen

--- lib/custom_widgets/subcategory_tile.dart
+++ lib/custom_widgets/subcategory_tile.dart
@@ -5,8 +5,6 @@ import 'package:smart_calculator/screens/age_calculator_screen.dart';
 import 'package:smart_calculator/screens/area_calculator_screen.dart';
 import 'package:smart_calculator/screens/average_calculator_screen.dart';
 import 'package:smart_calculator/screens/percentage_calculator_screen.dart';
-// import 'package:smart_calculator/screens/temparature_conversion_screen.dart';
-import 'package:smart_calculator/screens/time_intervel_srt';
 
 import '../../provider/theme_provider.dart';
 import '../data.dart';
@@ -18,6 +16,9 @@ import '../screens/length_convertor_screen.dart';
 import '../screens/proportion_calculator_screen.dart';
 import '../screens/ratios_calculator_screen.dart';
 import '../screens/temperature_conversion_screen.dart';
+import '../screens/time_intervel_screen.dart';
+import '../screens/volume_calculation_Screen.dart';
+import '../screens/volume_conversion_screen.dart';
 import '../screens/weight_conversion_screen.dart'; // Make sure subcategoryIcons and subcategoryColors are defined
 
 class SubcategoryTile extends StatelessWidget {
