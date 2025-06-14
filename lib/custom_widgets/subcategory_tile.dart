0813b3f4f4783572cef85311aa4956d852df1b34
ThogaruchestiHemanth <saihemanth225@gmail.com>
2025-06-14 15:32:07 +0530

    base64_converter update 2.0

--- lib/custom_widgets/subcategory_tile.dart
+++ lib/custom_widgets/subcategory_tile.dart
@@ -9,13 +9,14 @@ import 'package:smart_calculator/screens/loan_calculation_screen.dart';
 
 import '../../provider/theme_provider.dart';
 import '../data.dart';
+import '../functionalities/Base64_converter/base64_converter_screen.dart'
+    show Base64ConverterScreen;
 import '../screens/Equation_solver_screen.dart';
 import '../screens/acceleration_calculator_screen.dart';
 import '../screens/add_and_subtract_screen.dart';
 import '../screens/age_calculator_screen.dart';
 import '../screens/area_calculator_screen.dart';
 import '../screens/average_calculator_screen.dart';
-import '../screens/base64_screen.dart';
 import '../screens/bmi_screen.dart';
 import '../screens/data_storage_conversion_screen.dart';
 import '../screens/fraction_calculator_screen.dart';
